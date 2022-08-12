80a75606e7bb7ffc169166489e9c6a93162e472a
oladapo <oladapoelijah49@gmail.com>
2022-08-12 15:09:59 +0100

    contact form

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tutorial</title>
+</head>
+<body>
+    <h1>Hello world!</h1>
+
+    <?php
   echo 'hello world';
+  echo '<br>';
+  echo 'How are you doing?';
 ?>
+
+<?php
+$name = 'Oladapo ELijah';
+echo '<h1>My name is: '.$name.' </h1>';
+?>
+
+<?php
+ $string1 = "This is the first ";
+ $string2 = "part of my sentence.";
+
+ echo $string1.$string2
+
+?>
+</body>
+</html>
